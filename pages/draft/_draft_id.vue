b53b6d2c8b74e17808d9a3e61809d977e2f9a4c7
suwen <577014284@qq.com>
2021-02-26 11:14:26 +0800

    🩹 草稿不能编辑

--- pages/draft/_draft_id.vue
+++ pages/draft/_draft_id.vue
@@ -19,7 +19,8 @@
                 </div>
               </el-col>
               <el-col :xs="12" :sm="12" :xl="12" v-if="user" class="text-right">
-                <el-link rel="nofollow" :underline="false" title="总浏览数"><i class="el-icon-s-data"></i><span style="color: red;">{{ article.articleViewCount }}</span>
+                <el-link rel="nofollow" :underline="false" title="总浏览数"><i class="el-icon-s-data"></i><span
+                  style="color: red;">{{ article.articleViewCount }}</span>
                 </el-link>
               </el-col>
               <el-col style="margin: 1rem 0;">
@@ -33,7 +34,7 @@
                     # {{ tag.tagTitle }}
                   </el-tag>
                 </el-col>
-                <el-col v-if="user && user.idUser === article.articleAuthorId" :span="12" style="text-align: right;">
+                <el-col v-if="user && user.idUser == article.articleAuthorId" :span="12" style="text-align: right;">
                   <el-button size="mini" v-if="hasPermissions" @click="handleCommand('edit')" plain>编辑文章</el-button>
                 </el-col>
               </el-col>
@@ -65,110 +66,110 @@
 </template>
 
 <script>
-  import Vue from 'vue';
-  import {mapState} from 'vuex';
+import Vue from 'vue';
+import {mapState} from 'vuex';
 
-  export default {
-    name: "DraftDetail",
-    validate({params, store}) {
-      return params.draft_id && !isNaN(Number(params.draft_id))
-    },
-    fetch({store, params, error}) {
-      return Promise.all([
-        store
-          .dispatch('draft/fetchDetail', params)
-          .catch(err => error({statusCode: 404}))
-      ])
-    },
-    computed: {
-      ...mapState({
-        article: state => state.draft.detail.data,
-        isFetching: state => state.draft.detail.fetching,
-        isMobile: state => state.global.isMobile,
-        user: state => state.oauth,
-        avatar: state => state.userInfo?.avatarURL
-      }),
-      hasPermissions() {
-        let account = this.$store.state.userInfo?.nickname;
-        if (account) {
-          if (account === this.article?.articleAuthor?.userNickname) {
-            return true;
-          }
+export default {
+  name: "DraftDetail",
+  validate({params, store}) {
+    return params.draft_id && !isNaN(Number(params.draft_id))
+  },
+  fetch({store, params, error}) {
+    return Promise.all([
+      store
+        .dispatch('draft/fetchDetail', params)
+        .catch(err => error({statusCode: 404}))
+    ])
+  },
+  computed: {
+    ...mapState({
+      article: state => state.draft.detail.data,
+      isFetching: state => state.draft.detail.fetching,
+      isMobile: state => state.global.isMobile,
+      user: state => state.oauth,
+      avatar: state => state.userInfo?.avatarURL
+    }),
+    hasPermissions() {
+      let account = this.$store.state.userInfo?.nickname;
+      if (account) {
+        if (account === this.article?.articleAuthor?.userNickname) {
+          return true;
         }
-        return this.$store.getters.hasPermissions('blog_admin');
-      },
-      routeArticleId() {
-        return Number(this.$route.params.draft_id)
       }
+      return this.$store.getters.hasPermissions('blog_admin');
     },
-    data() {
-      return {
-        loading: false
-      }
-    },
-    methods: {
-      onRouter(name, data) {
-        this.$router.push(
-          {
-            path: `/${name}/${data}`
-          }
-        )
-      },
-      handleCommand(item) {
-        let _ts = this;
-        if (item === 'edit') {
-          _ts.$router.push({
-            path: `/article/post/${_ts.article.idArticle}`
-          })
-        } else {
-          _ts.$axios.$get('/api/article/' + _ts.article.idArticle + '/share').then(function (res) {
-            if (res) {
-              _ts.$set(_ts, 'shareData', res);
-              _ts.$set(_ts, 'isShare', true);
-            }
-          });
+    routeArticleId() {
+      return Number(this.$route.params.draft_id)
+    }
+  },
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    onRouter(name, data) {
+      this.$router.push(
+        {
+          path: `/${name}/${data}`
         }
-      },
-      gotoLogin() {
-        this.$router.push({
-          name: 'login'
+      )
+    },
+    handleCommand(item) {
+      let _ts = this;
+      if (item === 'edit') {
+        _ts.$router.push({
+          path: `/article/post/${_ts.article.idArticle}`
         })
+      } else {
+        _ts.$axios.$get('/api/article/' + _ts.article.idArticle + '/share').then(function (res) {
+          if (res) {
+            _ts.$set(_ts, 'shareData', res);
+            _ts.$set(_ts, 'isShare', true);
+          }
+        });
       }
     },
-    mounted() {
-      this.$store.commit('setActiveMenu', 'draftDetail');
-      Vue.nextTick(() => {
-        const previewElement = document.getElementById("articleContent");
-        // //const outLineElement = document.getElementById("articleToC");
-        // VditorPreview.setContentTheme('light');
-        Vue.VditorPreview.codeRender(previewElement, 'zh_CN');
-        Vue.VditorPreview.highlightRender({"enable": true, "lineNumber": false, "style": "github"}, previewElement);
-        Vue.VditorPreview.mathRender(previewElement, {
-          math: {"engine": "KaTeX", "inlineDigit": false, "macros": {}},
-        });
-        Vue.VditorPreview.mermaidRender(previewElement, ".language-mermaid");
-        Vue.VditorPreview.graphvizRender(previewElement);
-        Vue.VditorPreview.chartRender(previewElement);
-        Vue.VditorPreview.mindmapRender(previewElement);
-        Vue.VditorPreview.abcRender(previewElement);
-        Vue.VditorPreview.mediaRender(previewElement);
-        Vue.VditorPreview.lazyLoadImageRender(previewElement);
-        //VditorPreview.outlineRender(previewElement, outLineElement);
-        window.scrollTo(0,0);
+    gotoLogin() {
+      this.$router.push({
+        name: 'login'
       })
     }
+  },
+  mounted() {
+    this.$store.commit('setActiveMenu', 'draftDetail');
+    Vue.nextTick(() => {
+      const previewElement = document.getElementById("articleContent");
+      // //const outLineElement = document.getElementById("articleToC");
+      // VditorPreview.setContentTheme('light');
+      Vue.VditorPreview.codeRender(previewElement, 'zh_CN');
+      Vue.VditorPreview.highlightRender({"enable": true, "lineNumber": false, "style": "github"}, previewElement);
+      Vue.VditorPreview.mathRender(previewElement, {
+        math: {"engine": "KaTeX", "inlineDigit": false, "macros": {}},
+      });
+      Vue.VditorPreview.mermaidRender(previewElement, ".language-mermaid");
+      Vue.VditorPreview.graphvizRender(previewElement);
+      Vue.VditorPreview.chartRender(previewElement);
+      Vue.VditorPreview.mindmapRender(previewElement);
+      Vue.VditorPreview.abcRender(previewElement);
+      Vue.VditorPreview.mediaRender(previewElement);
+      Vue.VditorPreview.lazyLoadImageRender(previewElement);
+      //VditorPreview.outlineRender(previewElement, outLineElement);
+      window.scrollTo(0, 0);
+    })
   }
+}
 </script>
 
 <style lang="scss">
-  @import "~vditor/src/assets/scss/index.scss";
+@import "~vditor/src/assets/scss/index.scss";
 
-  .article__wrapper {
-    max-width: 980px;
-    margin: 0 auto;
-    display: block;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    box-sizing: border-box;
-  }
+.article__wrapper {
+  max-width: 980px;
+  margin: 0 auto;
+  display: block;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  box-sizing: border-box;
+}
 </style>
